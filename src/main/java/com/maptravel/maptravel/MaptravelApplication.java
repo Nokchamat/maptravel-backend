1aeef965ad60f3dfc17fc96f783d802126b210d2
NokchaMat <rkdtjd976@gmail.com>
2023-11-12 17:15:22 +0900

    Init

---
+++ src/main/java/com/maptravel/maptravel/MaptravelApplication.java
@@ -0,0 +1,13 @@
+package com.maptravel.maptravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptravelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MaptravelApplication.class, args);
+  }
+
+}
