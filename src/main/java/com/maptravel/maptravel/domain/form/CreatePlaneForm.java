bb131b76e472ca2b6574032eb0d1864372e6b679
Nokchamat <rkdtjd976@gmail.com>
2023-11-17 00:15:14 +0900

    # PlaneControllerTest - 생성, 조회, 디테일 조회, 삭제 성공 케이스 테스트 코드 작성

--- src/main/java/com/maptravel/maptravel/domain/form/CreatePlaneForm.java
+++ src/main/java/com/maptravel/maptravel/domain/form/CreatePlaneForm.java
@@ -2,17 +2,17 @@ package com.maptravel.maptravel.domain.form;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 import org.springframework.web.multipart.MultipartFile;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
+@Builder
 public class CreatePlaneForm {
 
   private String subject;
